02b7bbec7ad09ffcc4ef735b3b1ea4a98d1be199
Aquatic <not@email.address>
2024-07-18 20:23:49 +0900

    /heath_check 엔드포인트 작성

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use axum::{
     extract::Path,
+    http::StatusCode,
     response::{IntoResponse, Response},
     routing, Router,
 };
@@ -15,11 +16,16 @@ async fn greet(Path(map): Path<HashMap<String, String>>) -> Response {
     format!("Hello {}!", name).into_response()
 }
 
+// curl -v http://127.0.0.1:8000/health_check => 200 OK
+async fn health_check() -> Response {
+    StatusCode::OK.into_response()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = Router::new()
         .route("/", routing::get(greet))
-        .route("/:name", routing::get(greet));
+        .route("/health_check", routing::get(health_check));
     let tcp_listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await?;
     axum::serve(tcp_listener, app).await
 }
