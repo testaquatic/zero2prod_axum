1dc98db74359ded97d4fa4ef89817e312ffd3b94
TestAquatic <86832451+testaquatic@users.noreply.github.com>
2025-08-06 18:25:16 +0900

    /, /{name} 엔드포인트 작성

---
+++ README.md
@@ -0,0 +1,24 @@
+# zero2prod_axum
+
+["제로부터 시작하는 러스트 백엔드 프로그래밍"](https://product.kyobobook.co.kr/detail/S000212216062)을 읽으면서 작성한 코드이다.
+
+저자 깃허브 : [zero-to-production](https://github.com/LukeMathWalker/zero-to-production)
+
+[Actix Web](https://actix.rs/) 대신 [axum](https://github.com/tokio-rs/axum)을 사용했다.  
+내용이 알찬 책이다.
+
+## 실행 환경
+
+## API
+
+### /
+
+-   GET
+
+    Hello World
+
+### /{name}
+
+-   GET
+
+    Hello {name}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use axum::{
+    Router,
+    extract::{Path, Request},
+    routing::get,
+};
+
+/// [ axum::extract::path::Path ](<https://jabber-tools.github.io/google_cognitive_apis/doc/0.2.0/axum/extract/path/struct.Path.html>)
+/// 위 문서를 참고해서 작성했다.
+async fn greet(Path(params): Path<HashMap<String, String>>) -> String {
+    let name = params.get("name").cloned().unwrap_or("World".to_owned());
+    format!("Hello, {name}!")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/{name}", get(greet));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await?;
+    axum::serve(listener, app).await
 }
